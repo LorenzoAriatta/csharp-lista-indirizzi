0de87f3620dca0ac6cb9ee655556c29b4620da27
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-05 14:19:47 +0200

    set StreamReader to open csv file path

--- Program.cs
+++ Program.cs
@@ -5,3 +5,13 @@ In questo esercizio dovrete leggere un file CSV, non tanto differente da quanto 
 salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
 Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e
 di gestire come meglio crediate queste casistiche.
+*/
+
+StreamReader file = File.OpenText("C:\\.NET_projects\\csharp\\csharp-lista-indirizzi");
+
+string firstLine = file.ReadLine();
+
+while (!file.EndOfStream)
+{
+    string line = file.ReadLine();
+}
