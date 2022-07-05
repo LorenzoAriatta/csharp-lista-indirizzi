7f771fc2264b58ce5afec8b2d213c7709f9b9711
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-05 14:18:53 +0200

    create new class Address

---
+++ Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Address
+{
+	public string name = "";
+	public string surname = "";
+	public string street = "";
+	public string city = "";
+	public string province = "";
+	public int zip;
+	public Address()
+	{
+	}
+
+}
